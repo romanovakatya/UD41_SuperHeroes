fbbd99c2e071221fa8f52a40f4658d50f2a54d33
Katia <katerina.bogrom@gmail.com>
2021-09-06 22:12:20 +0200

    falta corregir errores

--- server/generate.js
+++ server/generate.js
@@ -2,6 +2,11 @@ const faker = require('faker');
 
 let database = { heroes: []};
 
+let date = new Date();
+const formatDate = (date)=>{
+  return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+}
+
 for (let i = 1; i <= 50; i++){
   database.heroes.push({
     id: i,
@@ -11,9 +16,9 @@ for (let i = 1; i <= 50; i++){
     role: faker.name.title(),
     new_role: faker.name.title(),
     type: faker.random.word(),
-    release_date: faker.date.past(),
-    rework_date: faker.date.future(),
-    attribute_id: faker.datatype.number,
+    release_date: faker.date.past(10, formatDate(date)),
+    rework_date: faker.date.future(10, formatDate(date)),
+    attribute_id: faker.datatype.number(),
     translations: [
       translation1 = faker.lorem.word(),
       translation1 = faker.lorem.word(),
--- src/app/components/heroe-details/heroe-details.component.ts
+++ src/app/components/heroe-details/heroe-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Heroe} from "../../models/heroe.model";
+import {HeroeService} from "../../services/heroe.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-heroe-details',
@@ -7,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentHeroe: Heroe = {
+
+    name: '',
+    short_name: '',
+    alt_name: '',
+    role: '',
+    new_role: '',
+    type: '',
+    release_date: '',
+    rework_date: '',
+    attribute_id: 0,
+    translations: ['']
+  };
+
+  message = '';
+
+  constructor(
+    private heroeService: HeroeService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getHeroe(this.route.snapshot.params.id);
   }
 
+  getHeroe(id: string): void {
+    this.heroeService.get(id)
+      .subscribe(
+        data => {
+          this.currentHeroe = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  updateHeroe(): void {
+    this.message = '';
+
+    this.heroeService.update(this.currentHeroe.id, this.currentHeroe)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message ?
+            response.message : "This superheroe was updated successfully!";
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  deleteHeroe(): void {
+    this.heroeService.delete(this.currentHeroe.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/books']);
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
 }
